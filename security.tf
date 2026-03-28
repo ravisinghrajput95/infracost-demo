76043042ec6f913065a2cd624a3409e6de4f27d3
Ravi Singh Rajput <92205137+ravisinghrajput95@users.noreply.github.com>
2026-03-28 15:07:36 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "demo_sg" {
+  name   = "demo-sg"
+  vpc_id = aws_vpc.demo_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
